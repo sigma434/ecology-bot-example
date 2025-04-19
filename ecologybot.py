8445fe0aa06661aa8ccea74a8ce4dcb3cae6b623
sigma434 <boleslavsem@gmail.com>
2025-04-19 15:34:02 +0300

    Create ecologybot.py

---
+++ ecologybot.py
@@ -0,0 +1,29 @@
+import telebot
+import os
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.handler_backends import ContinueHandling
+from telebot import types
+
+
+bot = telebot.TeleBot("7817058953:AAHHfuRIcY7Og5kCFj2EclAkiaQFYsflhS4")
+
+
+bot.set_my_commands(
+    commands=[
+        telebot.types.BotCommand("text", "sending you a text about ecology"),
+        telebot.types.BotCommand("start","greets you"),
+        ])
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я бот, который расскажет тебе про проблемы о нынешней экологии.")
+
+
+@bot.message_handler(commands=["text"])
+def text(message):
+    text=open("text.txt" ,"r",encoding="utf-8")
+    f=text.read()
+    bot.send_message(message.chat.id , f )
+    text.close()
+ 
